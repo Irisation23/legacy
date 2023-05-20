f752ab2a565c44352166e4fa161b5510515978fb
bunsung92 <bunsung92@naver.com>
2023-05-20 12:15:22 +0900

    aop.basic1 패키지의 코드를 추가하여 작업 After, Before 설정 #5

---
+++ src/main/java/com/example/legacy/aop/basic1/LoggerAdvice.java
@@ -0,0 +1,10 @@
+package com.example.legacy.aop.basic1;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LoggerAdvice {
+
+    public void loggerProcess(ProceedingJoinPoint jp) {
+
+    }
+}
